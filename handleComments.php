d78682879c4cd27567da5ed98f6257c6e9f84f64
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2021-04-01 13:34:17 +0200

    comment section fixed

--- handleComments.php
+++ handleComments.php
@@ -8,21 +8,24 @@ $password = $_SESSION["password"];
 
 
 
-/* if(!isset($_SESSION["username"])){
+ if(!isset($_SESSION["username"])){
     echo "FEL!!";
-}else {
-    $getSql= "SELECT ID FROM users WHERE Användarnamn=:username_IN AND Lösenord=:password_IN";
-    $stmt = $db->prepare($getSql);
-    $stmt->bindParam(":username_IN", $username);
-    $stmt->bindParam(":password_IN", $password);
-  $ID = $userID
+    die();
 }
- */
+
+$getSql= "SELECT ID FROM users WHERE Användarnamn=:username_IN AND Lösenord=:password_IN";
+$stmt = $db->prepare($getSql);
+$stmt->bindParam(":username_IN", $username);
+$stmt->bindParam(":password_IN", $password);
+$stmt->execute();
+$user_id = $stmt->fetch();
+
+ 
 
 $sql = "INSERT INTO entries(Meddelande, UserID) VALUES(:meddelande_IN, :UserID_IN)";
 $stm = $db->prepare($sql);
 $stm->bindParam(':meddelande_IN', $meddelande);
-$stm->bindParam(":UserID_IN", $_SESSION["ID"]);
+$stm->bindParam(":UserID_IN", $user_id[0]);
 
 
 if($stm->execute()) {
